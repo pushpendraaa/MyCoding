ecd2a869b6ccab2363284a871ca8f4a87ab18992
Pushpendra Kushwaha <89564501+pushpendraaa@users.noreply.github.com>
2024-01-01 09:44:41 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Transition Point/find-transition-point.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:    
+    
+    int transitionPoint(int arr[], int n) {
+        // code here
+        int l=0;
+        int h=n-1;
+        int ans=-1;
+        
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(arr[mid]==0){
+                l=mid+1;
+            }
+            else{
+                ans=mid;
+                h=mid-1;
+            }
+            
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int transitionPoint(int arr[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.transitionPoint(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
